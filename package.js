ad0e1300a228aa84cbafd1247f75124efe829a40
Bevan Hunt <me@bevanhunt.com>
2012-10-28 19:04:37 -0700

    change package to have one line add rather than two

--- package.js
+++ package.js
@@ -3,6 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('lib/sugar/release/1.3.5/sugar-1.3.5-full.development.js', 'client');
-  api.add_files('lib/sugar/release/1.3.5/sugar-1.3.5-full.development.js', 'server');
+  api.add_files('lib/sugar/release/1.3.5/sugar-1.3.5-full.development.js', ['client', 'server']);
 });
